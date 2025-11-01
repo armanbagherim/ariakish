69f6efd8f90e76808c38625cf2303e7301daa9a2
Arman <user@MacBookPro.lan>
2025-11-01 08:35:17 +0330

    add city to organizations

--- app/_components/design/NewAddress/index.tsx
+++ app/_components/design/NewAddress/index.tsx
@@ -3,12 +3,41 @@ import React from "react";
 import AdditionalData from "./AdditionalData";
 import Map from "./Map";
 
-function NewAddress({ formik, fetchAddress, shouldShowAddress, tempCity, proviences }) {
+interface NewAddressProps {
+  formik: any;
+  fetchAddress: () => Promise<void>;
+  shouldShowAddress: boolean;
+  tempCity: any;
+  proviences: Array<{id: number, name: string, slug: string}>;
+  cities: Array<{id: number, name: string}>;
+  loadingCities: boolean;
+  onProvinceChange: (value: any) => void;
+}
+
+function NewAddress({ 
+  formik, 
+  fetchAddress, 
+  shouldShowAddress, 
+  tempCity, 
+  proviences,
+  cities,
+  loadingCities,
+  onProvinceChange
+}: NewAddressProps) {
 
   return (
     <>
       <Map isAdmin data={formik} fetchAddress={fetchAddress} />
-      {shouldShowAddress && <AdditionalData proviences={proviences} tempCity={tempCity} data={formik} />}
+      {shouldShowAddress && (
+        <AdditionalData 
+          proviences={proviences} 
+          tempCity={tempCity} 
+          data={formik} 
+          cities={cities}
+          loadingCities={loadingCities}
+          onProvinceChange={onProvinceChange}
+        />
+      )}
     </>
   );
 }
